73b01b153364e8879ffc92f631239f1f3ef60e7f
Jan Tojnar <jtojnar@gmail.com>
2021-05-11 17:46:29 +0200

    spot: init at 0.1.14

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -24207,6 +24207,8 @@ in
 
   spectrwm = callPackage ../applications/window-managers/spectrwm { };
 
+  spot = callPackage ../applications/audio/spot { };
+
   spotify-cli-linux = callPackage ../applications/audio/spotify-cli-linux { };
 
   spotifyd = callPackage ../applications/audio/spotifyd {
